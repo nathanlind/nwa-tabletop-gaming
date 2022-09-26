5488b28e17726fc263ab40cea76e1dd7da989eba
nathanlind <nathan.nwa@gmail.com>
2022-09-25 19:48:17 -0500

    adds basic user login/register functionality

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'tg-nav',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  currentUser!: any;
+
+  signOut(): void {
+    this.currentUser = null;
+  }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.currentUser.subscribe(
+      user => this.currentUser = user
+    )
   }
 
 }
