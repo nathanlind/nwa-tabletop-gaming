3433a7184c57c2ed96045c433c490dc8e24d48a1
nathanlind <nathan.nwa@gmail.com>
2022-09-29 11:32:51 -0500

    refactor timeFormat pipe

--- src/app/pipes/timeFormat.pipe.ts
+++ src/app/pipes/timeFormat.pipe.ts
@@ -9,8 +9,7 @@ export class TimeFormatPipe implements PipeTransform {
     let hour = (time.split(':'))[0]
     let min = (time.split(':'))[1]
     let part = hour > 12 ? 'pm' : 'am';
-    if(parseInt(hour) == 0)
-    hour = 12;
+    if(parseInt(hour) == 0) hour = 12;
     min = (min+'').length == 1 ? `0${min}` : min;
     hour = hour > 12 ? hour - 12 : hour;
     hour = (hour+'').length == 1 ? `0${hour}` : hour;
